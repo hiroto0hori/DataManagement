627e08ac18b1dc46c562b0dd8deafaf08f0b4155
Hiroto OHORI <125386512+hiroto0hori@users.noreply.github.com>
2024-07-05 11:01:26 +0900

    HoT: initial revision

---
+++ ex04.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class HeadsOrTails {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int headsCount = 0;
+        int tailsCount = 0;
+        
+        System.out.println("Tossing a coin...");
+
+        for (int i = 1; i <= 3; i++) {
+            boolean isHeads = rand.nextBoolean();
+            if (isHeads) {
+                System.out.println("Round " + i + ": Heads");
+                headsCount++;
+            } else {
+                System.out.println("Round " + i + ": Tails");
+                tailsCount++;
+            }
+        }
+
+        System.out.println("Heads: " + headsCount + ", Tails: " + tailsCount);
+    }
+}
